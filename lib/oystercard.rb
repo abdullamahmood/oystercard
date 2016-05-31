754f9a1ea6afe5ef69e9d12cb75686e16cd9527c
Abdulla <abdulla.ali.mahmood@gmail.com>
2016-05-31 16:17:58 +0100

    step 8 done - touch in, out, in journey

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,37 @@
-
 class Oystercard
-end
+DEFAULT_BALANCE = 0
+MAX_LIMIT = 90
+
+  attr_reader :balance
+
+  def initialize(balance = DEFAULT_BALANCE)
+    @balance = balance
+  end
+
+	def top_up(amount)
+		message = "Maximum limit of #{Oystercard::MAX_LIMIT} reached"
+		raise message if amount + balance > MAX_LIMIT
+		@balance += amount
+		self
+	end
+
+	def deduct(amount)
+		@balance -= amount
+		self
+	end
+
+	def touch_in
+		@in_journey = true
+		@balance
+	end 
+	def touch_out
+		@in_journey = false
+		@balance
+	end
+
+	def in_journey?
+		@in_journey ||= false # ||= sets to false if it hasn't been initialized or set earlier (i.e. if it's equal nil)
+	end
+
+end 
+
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,45 @@
 require 'oystercard'
 
 describe Oystercard do	
+ subject(:oystercard) {described_class.new}
+
+
+	# it { is_expected.to respond_to(:top_up).with(1).argument }
+	describe "#top_up" do
+		it 'tops up the card and increases the balance' do
+			expect{oystercard.top_up(10)}.to change{oystercard.balance}.by 10
+			expect{oystercard.top_up(1)}.to change{oystercard.balance}.by 1
+		end
+		it 'has a maximum limit of 90' do
+			message = "Maximum limit of #{Oystercard::MAX_LIMIT} reached"
+			expect{oystercard.top_up(91)}.to raise_error message
+		end 
+	end
+	describe "#deduct" do
+		it 'deducts the money from the balance' do
+			expect{oystercard.deduct(10)}.to change{oystercard.balance}.by -10
+		end
+	end
+
+	describe "#touch_in" do
+		it 'touches in the station' do
+			expect(oystercard.touch_in).to eq oystercard.balance
+		end
+	end 
+
+	describe "#touch_out" do
+		it 'touches out of the station' do
+			expect(oystercard.touch_out).to eq oystercard.balance
+		end
+	end 
+
+	describe "#in_journey?" do
+		it 'should not be in journey when initialized' do
+			expect(oystercard).to_not be_in_journey
+		end
+		it 'should be in journey whe travel starts' do
+			oystercard.touch_in
+			expect(oystercard).to be_in_journey
+		end
+	end	
 end
